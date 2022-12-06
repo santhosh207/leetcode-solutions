f076c07b3467a9296f703a5709f17de6c3baaf7a
Santhosh S <ssanthu6494@gmail.com>
2022-12-06 22:10:10 +0530

    leetcode
    1. Two Sum

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int i=0,j=1;
+        vector<int> ans;
+        while(i<nums.size()){
+            if(j>=nums.size()){
+                i+=1;
+                j=i+1;
+            }
+            else if(nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                break;
+            }
+            
+            else{
+                j++;
+            }
+        }
+        return ans;
+        
+    }
+};
