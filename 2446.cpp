5567c979ab6098320a53c68163d1e2eb1e8f9871
Santhosh S <ssanthu6494@gmail.com>
2022-12-08 21:21:28 +0530

    leetcode
    2446. Determine if Two Events Have Conflict

---
+++ 2446.cpp
@@ -0,0 +1,10 @@
+/*2446. Determine if Two Events Have Conflict*/
+
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        if((event1[1]>=event2[0]) && (event1[0]<=event2[1]))
+            return true;
+       return false;
+    }
+};
